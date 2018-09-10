4abb832c08a34b6fe95c1e16549cb931de2a3d17
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-09-10 09:18:49 +0300

    add catalog in process

--- Domain/FieldType.cs
+++ Domain/FieldType.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Domain
 {
     public enum FieldType : byte
     {
+        [Description("Строка")]
         str = 0,
+        [Description("Булево")]
         boolean = 1,
+        [Description("Целое число")]
         integer = 2,
+        [Description("Дата")]
         date = 3
     }
 }
