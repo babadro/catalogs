e14be68b49d915745c20a06fc3630a2ae695abab
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-09-18 08:56:21 +0300

    Default Empty constructor for binding

--- Catalogs/Models/NewCatalogInput.cs
+++ Catalogs/Models/NewCatalogInput.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
--- Domain/Field.cs
+++ Domain/Field.cs
@@ -8,12 +8,6 @@ namespace Domain
 {
     public class Field
     {
-        public Field(int id, string name, FieldType ftype)
-        {
-            Id = id;
-            Name = name;
-            FieldType = ftype;
-        }
         public int Id { get; set; }
         public string Name { get; set; }
         public FieldType FieldType { get; set; }
