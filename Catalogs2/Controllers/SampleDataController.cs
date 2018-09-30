1960833db6b4f26aa3b7be78430e20bf6fbc526a
Andrey Petrovich <baba_dro@mail.ru>
2018-09-30 18:22:03 +0300

    stored procedure

--- Catalogs2/Controllers/SampleDataController.cs
+++ Catalogs2/Controllers/SampleDataController.cs
@@ -96,19 +96,10 @@ namespace Catalogs2.Controllers
         {
             using (IDbConnection db = _dbConnection)
             {
-                var catalogsAndVersions = db.Query<CatalogVersionInfo>(
-                    @"
-                        SELECT cat.catalog_name, cat.ID AS catalogId, ver.version_name, ver.ID AS versionId FROM
-                        (SELECT * FROM catalogs) cat
-                        LEFT JOIN
-                        (SELECT * FROM versions) ver
-                        ON
-                        cat.ID = ver.catalog_id
-                    "
-                ).GroupBy(info => info.CatalogId);
+                db.Query("[dbo].[Create_Catalog]", new { catalog_name = name }, commandType: CommandType.StoredProcedure).GroupBy(info => info.CatalogId);
             }
 
-            return null;
+            return new JsonResult(true);
         }
     }
 }
