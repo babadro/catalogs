d81b914bcbff21452075f9f9785a73d572bebd1b
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-10-12 07:41:36 +0300

    mock catalog table

--- Catalogs2/ClientApp/src/components/Catalog.js
+++ Catalogs2/ClientApp/src/components/Catalog.js
@@ -25,10 +25,96 @@ export class Catalog extends Component {
 
         return (
             <div>
-                <h1>Catalog view</h1>
-                <p>Catalog content</p>
-                {contents}
-            </div>
+                <br/>
+                <button type="button" className="btn btn-default">
+                    <span className="glyphicon glyphicon-search"></span> Apply filters
+                </button>
+                <button type="button" className="btn btn-danger">
+                    <span className="glyphicon"></span> Reset filters
+                </button>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">
+                            <div className="form-group">
+                                <input type="text" className="form-control" id="" placeholder="Строка"/>
+                            </div>
+                            Строка
+                        </th>
+                        <th scope="col">
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" name="survey" id="Radios1" value="Yes"/>
+                                    True
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" name="survey" id="Radios2" value="No"/>
+                                    False
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" name="survey" id="Radios3" value=""/>
+                                    All
+                                </label>
+                            </div>
+                            Булево
+                        </th>
+                        <th scope="col">
+                            <div className="form-group">
+                                From:
+                                <input type="number" className="form-control"/>
+                                To:
+                                <input type="number" className="form-control"/>
+                            </div>
+                            Число
+                        </th>
+                        <th scope="col">
+                            <div className="form-group">
+                                From:
+                                <input className="form-control" type="datetime-local" id=""/>
+                                To:
+                                <input className="form-control" type="datetime-local" id=""/>
+                            </div>
+                            Дата
+                        </th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Otto</td>
+                        <td>True</td>
+                        <td>311</td>
+                        <td>25 декабря</td>
+                        <td>
+                            <button type="button" className="btn btn-sm">Удаление</button>
+                        </td>
+                        <td>
+                            <button type="button" className="btn btn-sm">Публикация</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Otto</td>
+                        <td>True</td>
+                        <td>311</td>
+                        <td>25 декабря</td>
+                        <td>
+                            <button type="button" className="btn btn-sm">Удаление</button>
+                        </td>
+                        <td>
+                            <button type="button" className="btn btn-sm">Публикация</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         );
     }
 }
