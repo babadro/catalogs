16c36ef863fc0c15bae7898ed753e036ab6a1597
Andrey Petrovich <baba_dro@mail.ru>
2018-11-04 10:37:45 +0300

    create element form works

--- Catalogs2/ClientApp/src/components/CreateElement.js
+++ Catalogs2/ClientApp/src/components/CreateElement.js
@@ -6,7 +6,7 @@ export class CreateElement extends Component {
     constructor(props) {
         super(props);
         this.catalogId = this.props.match.params.catalogid;
-        this.state = { loading: false };
+        this.state = { loading: true };
 
         fetch('api/Element/Create/' + this.catalogId)
             .then(response => response.json())
@@ -15,14 +15,63 @@ export class CreateElement extends Component {
             });
     }
 
-    
+    renderForm(fields) {
+        return (
+            <form>
+                {fields.map(field =>
+                    <div className="form-row" key={field.id}>
+                        <div className="form-group">
+                            <label>{field.name}</label>
+                            {this.renderInput(field)}
+                        </div>
+                    </div>
+                )}
+            </form>
+        );
+    }
+
+    renderInput(field) {
+        switch (field.fieldType) {
+            case 0:
+                return (
+                    <input type="text" className="form-control" id="" placeholder="Строка" />
+                );
+                break;
+            case 1:
+                return (
+                    <div>
+                        <div className="radio">
+                            <label>
+                                <input type="radio" name="survey" id="Radios1" value="Yes" />
+                                True
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input type="radio" name="survey" id="Radios2" value="No" />
+                                False
+                            </label>
+                        </div>
+                    </div>
+                );
+            case 2:
+                return(
+                    <input type="number" className="form-control" />
+                );
+            case 3:
+                return (
+                    <input className="form-control" type="datetime-local" id="" />
+                );
+        }
+    }
 
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : <div>Таблица</div>
+        let contents = this.state.loading ? <p><em>Loading...</em></p> : this.renderForm(this.state.fields);
         return (
-            <div>{contents}</div>
+            <div>
+                <h1>Create new element</h1>
+                {contents}
+            </div>
         );
     }
 }
